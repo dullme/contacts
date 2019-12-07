76e2c741086ac278e73fb611afb7a0ff52b11f26
JINJIALEI <admin@dullme.com>
2019-12-07 21:52:57 +0800

    短信联系人

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -2,11 +2,36 @@
 
 namespace App\Admin\Controllers;
 
+use Carbon\Carbon;
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
+use Illuminate\Http\Request;
 
 
 class AuthController extends BaseAuthController
 {
+    public function postLogin(Request $request)
+    {
+        $this->loginValidator($request->all())->validate();
 
+        $credentials = $request->only([$this->username(), 'password']);
+        $remember = $request->get('remember', false);
+
+        if ($this->guard()->attempt($credentials, $remember)) {
+            if(Auth('admin')->user()->id != 1 && Carbon::parse(Auth('admin')->user()->expired_at)->lt(Carbon::now())){
+                $this->guard()->logout();
+
+                $request->session()->invalidate();
+
+                admin_toastr('登录失败！');
+
+                return abort(222);
+            }
+            return $this->sendLoginResponse($request);
+        }
+
+        return back()->withInput()->withErrors([
+            $this->username() => $this->getFailedLoginMessage(),
+        ]);
+    }
 }
 
---
+++ resources/views/errors/222.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('账号过期'))
+@section('code', '222')
+@section('message', __('账号过期'))
